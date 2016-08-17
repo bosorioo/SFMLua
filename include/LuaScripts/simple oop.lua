5dca38d49fd9190358b0d128edcbe8d715191e64
Bruno <osorio.bruno@hotmail.com>
2016-08-17 10:28:57 -0300

    Improved code for lua object orientation
    Renamed a few variable for better readability and a little performance
    increase.

--- include/LuaScripts/simple oop.lua
+++ include/LuaScripts/simple oop.lua
@@ -1,8 +1,12 @@
 local sub = string.sub
 local unpack = table.unpack
 local rawget = rawget
-local function class__call(class, ...) return class.new(...) end
-local function extends(derived, baseName) derived.baseClass = _G[baseName] end
+local function class__call(class, ...)
+	return class.new(...)
+end
+local function extends(derived, baseName)
+	derived._internal_baseClass = _G[baseName]
+end
 local propagationMeta = {}
 propagationMeta.__index = function(propagationTable, index)
 	local thisClass = propagationTable[1]
@@ -10,48 +14,48 @@ propagationMeta.__index = function(propagationTable, index)
 	if index == 'super' then
 		local ret = rawget(instance, index) or thisClass[index]
 		if ret then return ret end
-		propagationTable[1] = thisClass.baseClass
+		propagationTable[1] = thisClass._internal_baseClass
 		return propagationTable
 	end
 	return rawget(instance, index) or thisClass[index] or instance[index]
 end
 propagationMeta.__call = function(propagationTable, ...)
-	return propagationTable[1].constructor(propagationTable, ...)
+	return propagationTable[1].constructor(propagationTable[2], ...)
 end
 propagationMeta.__newindex = function(propagationTable, index, value)
 	propagationTable[2][index] = value
 end
 
 function Class (className)
-	local class = {baseClass = nil, extends = extends, name = className}
-	class.instance_meta = {
+	local class = {_internal_baseClass = nil, extends = extends}
+	class._internal_instance_meta = {
 		__index = function (instance, index)
 			local ret = rawget(instance, index) or class[index]
 			if ret then return ret end
-			local base = class.baseClass
+			local base = class._internal_baseClass
 			while base do
 				ret = base[index]
 				if ret then
 					return ret
 				end
-				base = base.baseClass
+				base = base._internal_baseClass
 			end
-			if index == 'super' and class.baseClass then
-				return setmetatable({class.baseClass, instance}, propagationMeta)
+			if index == 'super' and class._internal_baseClass then
+				return setmetatable({class._internal_baseClass, instance}, propagationMeta)
 			end
 		end,
 		__gc = function (instance)
 			if class.destructor then 
 				class.destructor(instance) 
-				if class.baseClass and class.baseClass.destructor then
-					class.baseClass.destructor(instance)
+				if class._internal_baseClass and class._internal_baseClass.destructor then
+					class._internal_baseClass.destructor(instance)
 				end
 			end 
 		end} 
 	class.new = function(...)
 		local args = {...} 
 		local instance = --[[class.baseClass and class.baseClass.new({}) or]] {}
-		setmetatable(instance, class.instance_meta) 
+		setmetatable(instance, class._internal_instance_meta) 
 		if class.constructor then
 			local construction = class.constructor(instance, unpack(args, args[1] == class and 2 or 1))
 			if construction ~= nil then
@@ -64,7 +68,7 @@ function Class (className)
 	class.getClassName = getName class.typeof = className
 	setmetatable(class, {__newindex = function (class, index, value)
 		if type(index) == 'string' and sub(index, 1, 2) == '__' then
-			rawset(class.instance_meta, index, value)
+			rawset(class._internal_instance_meta, index, value)
 		else 
 			rawset(class, index, value)
 		end 
@@ -88,5 +92,5 @@ function typeof(thing, check)
 		return t == check
 	end
 	
-return t == 'table' and thing.getClassName and thing.getClassName() or t
+	return t == 'table' and thing.getClassName and thing.getClassName() or t
 end
